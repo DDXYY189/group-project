12f32007728a179238630fd624a7a16ce74e63de
yyxq <3330727250@qq.com>
2026-08-14 21:45:03 +0800

    初始化SpringBoot项目JDK21Maven

---
+++ demo-wkx/src/main/java/com/example/demo_wkx/DemoWkxApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_wkx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWkxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWkxApplication.class, args);
+	}
+
+}
